e217269bf4a71300f157d7f3d9082085e0f3fd94
novb1492 <novb1492@naver.com>
2021-12-10 11:36:11 +0900

    aws sqs 연동성공

---
+++ src/main/java/com/sesi/studyunity2_back/demo/config/sqsConfig.java
@@ -0,0 +1,33 @@
+package com.sesi.studyunity2_back.demo.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+
+@Configuration
+public class sqsConfig {
+    
+    private final String acce="AKIAXVQHL24PPX3ID5PF";
+    private final String sec="N2IzY6A5Zw+p92KvnWi41PaI7k0/fUhIhZ8IiQNg";
+
+    @Bean
+    public QueueMessagingTemplate queueMessagingTemplate() {
+        return new QueueMessagingTemplate(amazonSQSAsync());
+    }
+
+    @Primary
+    @Bean
+    public AmazonSQSAsync amazonSQSAsync() {
+        return AmazonSQSAsyncClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_2).withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(acce, sec))).build();
+    }
+
+}
--- src/main/java/com/sesi/studyunity2_back/demo/controller/restController.java
+++ src/main/java/com/sesi/studyunity2_back/demo/controller/restController.java
@@ -8,8 +8,10 @@ import com.sesi.studyunity2_back.demo.service.kakaoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,8 @@ public class restController {
     private kakaoService kakaoService;
     @Autowired
     private RedisTemplate<String,String>redisTemplate;
+    @Autowired
+    private QueueMessagingTemplate queueMessagingTemplate;
 
     @RequestMapping(value = "/api/kakao/**",method = RequestMethod.GET)
     public String callApi(HttpServletRequest request,HttpServletResponse response) {
@@ -41,5 +45,13 @@ public class restController {
 
         String s=valueOperations.get("b");
         logger.info(s);
+    }
+    @RequestMapping(value = "/message",method =RequestMethod.POST )
+    public void sendSqs(HttpServletRequest request,HttpServletResponse response) {
+        logger.info("sendSqs");
+        String url="https://sqs.ap-northeast-2.amazonaws.com/527222691614/testsqs";
+        String message=request.getParameter("message");
+        queueMessagingTemplate.send(url,MessageBuilder.withPayload(message).build());
+        
     }
 }
